638fb1c025500e3106241960d4ac9cbdb256ea1c
alan-botts <alan-botts@users.noreply.github.com>
2026-05-23 17:58:47 +0000

    Add text-scale meta tag sitewide

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -53153,6 +53153,7 @@ var baseHTML = `<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="text-scale" content="scale">
 <link rel="icon" type="image/svg+xml" href="/favicon.svg">
 <title>Agent RPG</title>
 <style>
